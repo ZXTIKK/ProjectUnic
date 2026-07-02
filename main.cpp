3685ab0a0957e920e065cd7d6664ac25d8450016
Maksimum423 <podlesnyjmaksim321@gmail.com.com>
2026-07-02 16:28:42 +0300

    addIcon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "MainWindow/mainwindow.h"
-
 #include <QApplication>
+#include <QIcon>
+#include <cstdio>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+#ifdef NDEBUG
+    std::freopen("nul", "w", stdout);
+    std::freopen("nul", "w", stderr);
+    std::ios_base::sync_with_stdio(false);
+#endif
     QApplication a(argc, argv);
+
+    a.setWindowIcon(QIcon("/Res/iconApp.png"));
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
